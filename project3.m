68e179d83589e9237f4cd227549cf4e99aec9c58
maticstare <maticstare8@gmail.com>
2024-01-21 18:05:56 +0100

    Cleared up the repo, added r squared logic.

--- project3.m
+++ project3.m
@@ -1,32 +1,72 @@
 %Author: Matic Stare
 
-%subject 20
-[sigs,freq,tm]=rdsamp('eegmmidb/S020/S020R01.edf');
-%exclude the last signal
-sigs = sigs(:, 1:64);
+%baseline eyes open: S020R01.edf
+%baseline eyes closed: S020R02.edf
+%task1: S020R03.edf
+%task1: S020R07.edf
+%task1: S020R11.edf
 
-%figure1
-figure("Name", "Začetni signali");
-plot(tm, sigs)
 
-%figure2
-figure("Name", "Začetni posamezni signali");
-for i=1:64
-    subplot(8,8,i);
-    plot(tm, sigs(:,i));
-    title(strcat("Komponenta ", int2str(i)));
+n_of_freq_bands = 10;
+
+baselineOpen = read_data('eegmmidb/S020/S020R01.edf');
+%baselineClosed = read_data('eegmmidb/S020/S020R02.edf');
+task11 = read_data('eegmmidb/S020/S020R03.edf');
+task12 = read_data('eegmmidb/S020/S020R07.edf');
+task13 = read_data('eegmmidb/S020/S020R11.edf');
+
+size_of_baseline = size(baselineOpen, 1);
+
+task = zeros(size_of_baseline, 64, 3);
+task(:, :, 1) = task11(1:size_of_baseline, :);
+task(:, :, 2) = task12(1:size_of_baseline, :);
+task(:, :, 3) = task13(1:size_of_baseline, :);
+
+mean_of_task = mean(task, 3);
+
+freq_bands_of_task = split_freq_bands(mean_of_task, n_of_freq_bands);
+
+
+
+Rsquared = zeros(64, n_of_freq_bands);
+for i = 1:64
+    for j = 1:n_of_freq_bands
+        Rsquared(i, j) = r2(baselineOpen(:, i), freq_bands_of_task(:, i, j));
+    end
 end
 
-%nastavitev maksimalnega števila iteracij na 5000
-[icasig,A,W] = fastica(sigs', 'maxNumIterations', 5000);
+hmap = heatmap(Rsquared);
+hmap.Title = 'R kvadrat';
+hmap.XLabel = 'Frekvenčni pas';
+hmap.YLabel = 'Kanal elektrode';
 
-sigsToRemove = input("Vnesi polje signalov, ki jih želiš odstraniti: ");
+%functions
+function sigs = read_data(path)
+    [sigs,~,~] = rdsamp(path);
+    sigs = sigs(:, 1:64);
+end
 
-%sigsToRemove = 1:16; %signali nižjega indeksa imajo večji vpliv
-A(:, sigsToRemove) = 0;
+function out = split_freq_bands(sigs, n_of_bands)
+    len_of_sigs = size(sigs, 1);
+    size_of_each_band = round(len_of_sigs / n_of_bands);
+    fourier = fft(sigs);
+    freq_split = zeros(len_of_sigs, 64, n_of_bands);
 
-newSigs = (A*icasig)';
+    for i = 1 : n_of_bands
+        firstI = (i-1) * size_of_each_band + 1;
+        lastI = firstI + size_of_each_band - 1;
+        freq_split(firstI:lastI, :, i) = fourier(firstI:lastI, :);
+    end
 
-%figure3
-figure("Name", "Signali po odstranitvi očesnih artefaktov");
-plot(tm, newSigs)
+    out = zeros(len_of_sigs, 64, n_of_bands);
+    for i = 1 : n_of_bands
+        out(:, :, i) = ifft(freq_split(:, :, i));
+    end
+    out = real(out);
+end
+
+function out = r2(x, y)
+    lin_model = fitlm(x, y);
+    out = lin_model.Rsquared.Ordinary;
+end
+
